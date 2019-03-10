4507682c42e3db5a34041760aee67dc3dacb48bf
Joseph Scott <jbs198585@gmail.com>
2019-03-09 19:37:44 -0500

    second method

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -83,7 +83,16 @@ public class SpeedConverter {
             return -1;
         }
 
-        long milesPerHour = Math.round(kilometersPerHour / 1.609);
-        return milesPerHour;
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if(kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + "km/h= " + milesPerHour + "mi/h");
+        }
     }
 }
